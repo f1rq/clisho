b683a5f77dd9397957b27bad4ba5165e1efb612c
f1rq <f.czudaj@tutanota.com>
2026-01-22 15:43:25 +0100

    feat: add simple connectivity with the api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-}
+use crate::models::JishoResponse;
 
+mod models;
+
+#[tokio::main]
+async fn main() {
+    match JishoResponse::search("hito").await {
+        Ok(res) => {
+            for word in res.data {
+                println!("Word: {}", word.slug);
+
+                if let Some(sense) = word.senses.first() {
+                    println!("Meaning: {:?}", sense.english_definitions);
+                }
+                println!("---");
+            }
+        }
+        Err(e) => eprint!("Błąd: {}", e),
+    }
+}
---
+++ src/models.rs
@@ -0,0 +1,71 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct JishoResponse {
+    pub meta: Meta,
+    pub data: Vec<WordData>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Meta {
+    pub status: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WordData {
+    pub slug: String,
+    pub is_common: bool,
+    pub tags: Vec<String>,
+    pub jlpt: Vec<String>,
+    pub japanese: Vec<JapaneseSource>,
+    pub senses: Vec<Sense>,
+    pub attribution: Attribution,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Sense {
+    pub english_definitions: Vec<String>,
+    pub parts_of_speech: Vec<String>,
+    pub links: Vec<Link>, 
+    pub tags: Vec<String>,
+    pub restrictions: Vec<String>,
+    pub see_also: Vec<String>,
+    pub antonyms: Vec<String>,
+    pub source: Vec<String>,
+    pub info: Vec<String>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct JapaneseSource {
+    pub word: Option<String>,
+    pub reading: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Attribution {
+    pub jmdict: bool,
+    pub jmnedict: bool,
+    pub dbpedia: serde_json::Value,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Link {
+    pub text: String,
+    pub url: String,
+}
+
+impl JishoResponse {
+    pub async fn search(keyword: &str) -> Result<Self, reqwest::Error> {
+        let url = format!("https://jisho.org/api/v1/search/words?keyword={}", keyword);
+
+        let client = reqwest::Client::new();
+        let response = client
+            .get(url)
+            .send()
+            .await?
+            .json::<Self>()
+            .await?;
+
+        Ok(response)
+    }
+}
